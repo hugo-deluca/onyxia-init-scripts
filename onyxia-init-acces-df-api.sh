1751144b1f65d38b1d413f5c0d0f64ed816c7470
hugo-deluca <94049752+hugo-deluca@users.noreply.github.com>
2024-04-10 16:54:08 +0200

    Create onyxia-init-acces-df-api.sh

---
+++ onyxia-init-acces-df-api.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Clone the repo
+git clone https://$1@gitlab.cerema.fr/datafoncier/acces-df/acces_df_api.git
+
+# Install modules in venv
+cd acces_df_api
+pip install -r requirements.txt
+
+# Creating env file
+DOMAIN=$(echo "$VSCODE_PROXY_URI" | awk -F/ '{print $3}')
+SECONDARY_LINK="${NOPROXY_URI%.*}-user.${domain#*.}"
+
+KEY=$(tr -dc "a-zA-Z0-9" < /dev/urandom | fold -w 32 | head -1)
+
+cat > acces_df_api/.env << EOF
+SECRET_KEY=$KEY
+DJANGO_ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS $DOMAIN $SECONDARY_LINK
+PRODUCTION=$PRODUCTION
+STATIC_URL=/static/
+SQL_ENGINE=$SQL_ENGINE
+SQL_HOST=$SQL_HOST
+SQL_DATABASE=$SQL_DATABASE
+SQL_PORT=$SQL_PORT
+SQL_USER=$SQL_USER
+SQL_PASSWORD=$SQL_PASSWORD
+EOF
