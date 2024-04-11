3b779ca220b49ecdf4c71c6dd37831fecb66e079
hugo-deluca <94049752+hugo-deluca@users.noreply.github.com>
2024-04-11 08:43:42 +0200

    Create onyxia-init-managerdf.sh

---
+++ onyxia-init-managerdf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install redis
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+
+sudo apt-get update
+sudo apt-get install redis
+
+
+# Clone the repo
+git clone https://$GITHUB_TOKEN@github.com/aherman59/managerdf.git
+
+
+# Install modules in venv
+cd managerdf
+pip install -r requirements.
+
+
+# Run API Livraisons server
+# uvicorn api.main:app --reload
